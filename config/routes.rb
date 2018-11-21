9f858ceb5673586d0ef8dd18fb668f32f18597be
Mehdi Ritab <ritabmehdi@gmail.com>
2018-11-21 18:02:15 +0100

    commiting the rails yelp mvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, except: [:update, :destroy]
+  resources :reviews, except: [:update, :destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = Restaurant.create(
+    { name: 'Mcdo', address: 'Ain Diab', category: 'french' },
+    { name: 'PizzaHut', address: 'Ain Diab', category: 'italien' },
+    { name: 'SuchiChina', address: 'Ghandi', category: 'chinese' },
+    { name: 'Yuki', address: 'Anfa', category: 'japanese' },
+    { name: 'LuxRest', address: 'Maarif', category: 'belgian' }
+  )
+
